406907011bf23d1b0e6fbb21e04a7d1512268c80
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-07-03 15:46:21 +0300

    Add project files.

---
+++ Lab3ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ex 3
+// o functie care va determina daca un numar este sau nu patrat perfect. Apelati-o si afisati-i rezultatu
+//
+//
+
+
+
+
+
+Console.WriteLine("Introduce un numar");
+int numar = int.Parse(Console.ReadLine());
+
+bool rezultat = IsPatratPerfect(numar);
+
+if (rezultat)
+    Console.WriteLine("Numărul este un pătrat perfect.");
+else
+    Console.WriteLine("Numărul nu este un pătrat perfect.");
+
+
+static bool IsPatratPerfect(int numar)
+{
+    int radacina = (int)Math.Sqrt(numar);
+    return radacina * radacina == numar;
+}
